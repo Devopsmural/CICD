706c79b54e6de5ec30b88af1a3b7c9cc82b48962
Devopsmural <61749474+Devopsmural@users.noreply.github.com>
2021-06-15 10:45:18 +0530

    Update rename.sh

--- CICD/rename.sh
+++ CICD/rename.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Creating a new Dir to move the existing .jar file"
-mv /home/ubuntu/murali/CICD/workspace/CICD/CICD/samplejar/target/demo-1.0-SNAPSHOT.jar /home/ubuntu/krish/demo-1.0-SNAPSHOT-$(date +%F-%H:%M).jar
+mv /tmp/slave1/CICD/CICD/samplejar/target/demo-1.0-SNAPSHOT.jar /tmp/slave1/rename/demo-1.0-SNAPSHOT-$(date +%F-%H:%M).jar
