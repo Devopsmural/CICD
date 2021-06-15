6d38ed92474820959aff1f82afdbfe093524dbf1
murali <anemkrishna8@gmail.com>
2021-06-15 17:50:30 +0000

    neww modification

--- CICD/rename.sh
+++ CICD/rename.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "Creating a new Dir to move the existing .jar file"
-mkdir -p /tmp/slave1/rename_war/
-mv /tmp/slave1/workspace/Demo_Pipeline/CICD/samplejar/target/demo-1.0-SNAPSHOT.jar /tmp/slave1/rename_war/demo-1.0-SNAPSHOT-$(date +%F-%H:%M).jar
+echo "Creating a new Dir to move the existing .war file"
+mkdir -p /tmp/slave1/snap-shot-war/
+mv /tmp/slave1/workspace/Demo_Pipeline/CICD/samplewar/target/samplewar.war /tmp/slave1/snap-shot-war/samplewar-$(date +%F-%H:%M).war
