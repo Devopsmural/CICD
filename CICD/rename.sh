e269dfb114f8d0697ca35dd94c613532430a7706
murali <anemkrishna8@gmail.com>
2021-06-20 07:44:55 +0000

    new

--- CICD/rename.sh
+++ CICD/rename.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Creating a new Dir to move the existing .war file"
-mkdir -p /tmp/slave1/workspace/Demo_Pipeline/snap-shot-war/
+mkdir -p /tmp/slave1/snap-shot-war/
 cp /tmp/slave1/workspace/Demo_Pipeline/CICD/samplewar/target/samplewar.war /tmp/slave1/snap-shot-war/samplewar-$(date +%F-%H:%M).war
